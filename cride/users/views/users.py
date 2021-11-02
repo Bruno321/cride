bfd911694e3a22f70272f17cde464f5d665d2033
Buno Paz <brunopaz00@yahoo.com.mx>
2021-11-02 16:01:47 -0400

    User and profile patch fixed

--- cride/users/views/users.py
+++ cride/users/views/users.py
@@ -1,88 +1,86 @@
-"""users view"""
+"""Users views."""
 
-#django rest framework
-from rest_framework import status, viewsets, mixins
+# Django REST Framework
+from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-# permissions
+# Permissions
 from rest_framework.permissions import (
     AllowAny,
     IsAuthenticated
 )
 from cride.users.permissions import IsAccountOwner
 
-
-# serializers
+# Serializers
+from cride.users.serializers.profiles import ProfileModelSerializer
 from cride.circles.serializers import CircleModelSerializer
 from cride.users.serializers import (
-    UserLoginSerializer, 
+    AccountVerificationSerializer,
+    UserLoginSerializer,
     UserModelSerializer,
-    UserSignUpSerializer,
-    AccountVerificationSerializer
+    UserSignUpSerializer
 )
 
+# Models
 from cride.users.models import User
 from cride.circles.models import Circle
 
-from cride.users.serializers.profiles import ProfileModelSerializer
 
+class UserViewSet(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  viewsets.GenericViewSet):
+    """User view set.
+    Handle sign up, login and account verification.
+    """
 
-class UserViewSet(
-    mixins.RetrieveModelMixin, 
-    mixins.UpdateModelMixin,
-    viewsets.GenericViewSet):
-
-    # que modelo usara para ser listado
-    queryset = User.objects.filter(is_active=True,is_client=True)
-    # que serializer usara
+    queryset = User.objects.filter(is_active=True, is_client=True)
     serializer_class = UserModelSerializer
     lookup_field = 'username'
 
     def get_permissions(self):
-        if self.action in ['signup','login','verify']:
+        """Assign permissions based on action."""
+        if self.action in ['signup', 'login', 'verify']:
             permissions = [AllowAny]
-        elif self.action == ['retrieve','update','partial_update']:
-            permissions = [IsAuthenticated,IsAccountOwner]
+        elif self.action in ['retrieve', 'update', 'partial_update', 'profile']:
+            permissions = [IsAuthenticated, IsAccountOwner]
         else:
             permissions = [IsAuthenticated]
         return [p() for p in permissions]
 
     @action(detail=False, methods=['post'])
-    def login(self,request):
+    def login(self, request):
+        """User sign in."""
         serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user,token = serializer.save()
+        user, token = serializer.save()
         data = {
             'user': UserModelSerializer(user).data,
-            'access_token':token
+            'access_token': token
         }
         return Response(data, status=status.HTTP_201_CREATED)
 
     @action(detail=False, methods=['post'])
-    def signup(self,request):
-        """handle http post request"""
+    def signup(self, request):
+        """User sign up."""
         serializer = UserSignUpSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         data = UserModelSerializer(user).data
-            
         return Response(data, status=status.HTTP_201_CREATED)
 
-    @action(detail=False,methods=['post'])
-    def verify(self,request):
-        """handle http post request"""
+    @action(detail=False, methods=['post'])
+    def verify(self, request):
+        """Account verification."""
         serializer = AccountVerificationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        data = {
-            'message': 'congratulation go and share rides'
-        }
-            
+        data = {'message': 'Congratulation, now go share some rides!'}
         return Response(data, status=status.HTTP_200_OK)
 
-    @action(detail=True,methods=['put','patch'])
-    def profile(self,request,*args,**kwargs):
+    @action(detail=True, methods=['put', 'patch'])
+    def profile(self, request, *args, **kwargs):
+        """Update profile data."""
         user = self.get_object()
         profile = user.profile
         partial = request.method == 'PATCH'
@@ -96,16 +94,16 @@ class UserViewSet(
         data = UserModelSerializer(user).data
         return Response(data)
 
-    def retrieve(self,request,*args,**kwargs):
-        response = super(UserViewSet, self).retrieve(request,*args,*kwargs)
+    def retrieve(self, request, *args, **kwargs):
+        """Add extra data to the response."""
+        response = super(UserViewSet, self).retrieve(request, *args, **kwargs)
         circles = Circle.objects.filter(
             members=request.user,
-            membership__is_active=True,
-            is_active=True
+            membership__is_active=True
         )
         data = {
-            'user':response.data,
-            'circles':CircleModelSerializer(circles, many=True).data
+            'user': response.data,
+            'circles': CircleModelSerializer(circles, many=True).data
         }
         response.data = data
         return response
